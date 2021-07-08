dbbdd0c4b5525ee30287b3237423750488e12997
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-07-08 12:52:01 -0300

    Traduzindo para ingles, e finalizando ultimo cap.

---
+++ games/hangman.py
@@ -0,0 +1,11 @@
+
+def play():
+    print("*****************************")
+    print("***Welcome to the Hangman!***")
+    print("*****************************")
+
+    print("Game Over")
+
+
+if(__name__ == "__main__"):
+    play()
