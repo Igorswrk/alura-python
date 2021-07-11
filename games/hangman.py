1e37185cd3244e15f0652edfbe1e52537c7bce26
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-07-11 02:38:51 -0300

    Finalizando 2-cap do 2-mod na formacao em python

--- games/hangman.py
+++ games/hangman.py
@@ -10,6 +10,15 @@ def play():
     hit = False
 
     while((not hanged) and (not hit)):
+
+        guess = input("Which letter?")
+        guess = guess.strip()
+
+        index = 0
+        for letter in secrect_word:
+            if(guess.upper() == letter.upper()):
+                print("I found the letter {} in position {}".format(letter, index))
+            index = index + 1
         print("Playing...")
 
     print("Game Over")
