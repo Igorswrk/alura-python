87a5adc11673b7bab344ce7d92c87555ac9ae08d
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-07-16 03:58:18 -0300

    Finalizando 5-cap do 2-mod na formacao em python

--- games/hangman.py
+++ games/hangman.py
@@ -4,25 +4,37 @@ def play():
     print("***Welcome to the Hangman!***")
     print("*****************************")
 
-    secrect_word = "banana"
-    letter_board = ["_", "_", "_", "_", "_", "_"]
+    secrect_word = "banana".upper()
+    letter_board = ["_" for letter in secrect_word]
 
     hanged = False
     hit = False
+    mistakes = 0
 
     print(letter_board)
     
-    while((not hanged) and (not hit)):
+    while(not hanged and not hit):
 
         guess = input("Which letter?")
-        guess = guess.strip()
+        guess = guess.strip().upper()
 
-        index = 0
-        for letter in secrect_word:
-            if(guess.upper() == letter.upper()):
-                letter_board[index] = letter
-            index = index + 1
+        if(guess in secrect_word):
+            index = 0
+            for letter in secrect_word:
+                if(guess == letter):
+                    letter_board[index] = letter
+                index += 1
+        else:
+            mistakes += 1
+
+        hanged = mistakes == 6
+        hit = "_" not in letter_board
         print(letter_board)
+
+        if(hit):
+            print("You win!")
+        elif(hanged):
+            print("You lose!")
 
     print("Game Over")
 
