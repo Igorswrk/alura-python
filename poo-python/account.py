d4cc77ece8c22fc41a60259149bfeaa78831d6a4
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-07-21 01:54:33 -0300

    finalizando mod3-capitulo-3 POO-python

--- poo-python/account.py
+++ poo-python/account.py
@@ -1,8 +1,17 @@
-
 class Account:
     def __init__(self, number, user, balance, limit):
-        print(f'construindo objeto {self}')
+        print(f'building object {self}')
         self.number = number  # Atributos
         self.user = user
         self.balance = balance
         self.limit = limit
+
+    def deposit(self, value):
+        self.balance += value
+
+    def withdraw(self, value):
+        self.balance -= value
+
+    def statement(self):
+        print(f"balance {self.balance} of the user {self.user}")
+
