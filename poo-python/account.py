28aafa848e5072931c0ad560c02f7d91b19cf8ed
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-07-23 02:36:18 -0300

    finalizando 3-modulo poo-python

--- poo-python/account.py
+++ poo-python/account.py
@@ -10,7 +10,14 @@ class Account:
         self.__balance += value
 
     def withdraw(self, value):
-        self.__balance -= value
+        if(self.__can_draw(value)):
+            self.__balance -= value
+        else:
+            print(f"The value {value} exceeds the limit")
+
+    def __can_draw(self, value):
+        balance_available_to_withdraw = self.__balance + self.__limit
+        return value <= balance_available_to_withdraw
 
     def statement(self):
         print(f"balance {self.__balance} of the user {self.__user}")
@@ -34,3 +41,11 @@ class Account:
     @limit.setter
     def limit(self, new_limit):
         self.__limit = new_limit
+
+    @staticmethod
+    def bank_code():
+        return "001"
+
+    @staticmethod
+    def bank_codes():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
