37a4dc08015984baf9e2bc3288c34ce219ddc75e
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-07-28 02:19:14 -0300

    finalizando 4-mod-cap1 poo-python-02 - revisao

--- poo-python-02/model.py
+++ poo-python-02/model.py
@@ -1,15 +1,47 @@
 class Film:
     def __init__(self, name, year, running_time):
-        self.name = name
+        self.__name = name.title()
         self.year = year
         self.running_time = running_time
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def add_likes(self):
+        self.__likes += 1
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
 
 
 class Series:
     def __init__(self, name, year, seasons):
-        self.name = name
+        self.__name = name.title()
         self.year = year
         self.seasons = seasons
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def add_likes(self):
+        self.__likes += 1
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
 
 
 avengers = Film('avengers - infinity war', 2018, 160)
