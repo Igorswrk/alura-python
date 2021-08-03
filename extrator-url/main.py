8e800b5c9c18b405d2032af05584773e127e8500
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-08-03 05:54:45 -0300

    finalizando mod5-cap1 fatiamento de url

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,1 +1,9 @@
+url = "bytebank.com/cambio?moedaOrigem=real"  # Strings no Python são objetos imutáveis!
+print(url)
 
+
+url_base = url[0:19]
+print(url_base)
+
+url_parameters = url[20:36]
+print(url_parameters)
