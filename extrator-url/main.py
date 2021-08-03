c33be510eb939680461d034c0975c5b10ca04daa
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-08-03 05:57:09 -0300

    add comentarios

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -7,3 +7,12 @@ print(url_base)
 
 url_parameters = url[20:36]
 print(url_parameters)
+
+"""
+URLs e seus formatos: como as URLs funcionam e o que cada parte de uma URL significa - base e parâmetros.
+
+O operador de fatiamento [a:b], utilizado para obter uma substring desde o índice a até o índice b - 1 da string 
+original. Lembrando que b - 1 pois o segundo argumento do fatiamento é exclusivo.
+
+A string original não é alterada ao ser fatiada devido à sua imutabilidade.
+"""
