55e66ea418dd081d91ec9bdb6402baff090b772b
Igorswrk <igor.paixao@aluno.ufop.edu.br>
2021-08-04 05:42:19 -0300

    finalizando mod5-cap6 metodos especiais

--- extrator-url/extractor_url.py
+++ extrator-url/extractor_url.py
@@ -40,8 +40,45 @@ class ExtractURL:
             value = self.get_parameters_url()[index_value:index_ampersand]
         return value
 
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return "URL: " + self.url + "\n" + "Parametros: " + self.get_parameters_url() + "\n" + "URL_BASE: " + self.get_base_url()
+
+    def __eq__(self, other):
+        return self.url == other.url
+
 
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+
 extractor_url = ExtractURL(url)
+
+# Desafio conversao Real para o Dolar
+VALOR_DOLAR = 5.50
+
+coin_origin = extractor_url.get_parameters_value("moedaOrigem")
+print(f"Moeda Origem: {coin_origin.title()}")
+
+coin_destiny = extractor_url.get_parameters_value("moedaDestino")
+print(f"Moeda Destino: {coin_destiny.title()}")
+
 value_qtd = extractor_url.get_parameters_value("quantidade")
-print(value_qtd)
+print(f"Valor: R$ {value_qtd}")
+
+conversion = float(value_qtd) * VALOR_DOLAR
+print(f"R$ {value_qtd} = $ {conversion}")
+
+# extractor_url_2 = ExtractURL(url)
+
+# print(extractor_url == extractor_url_2)  # extractor_url.__eq__(extractor_url_2)
+
+# print("O tamanho da URL:", len(extractor_url))
+# print(extractor_url)
+
+# print(id(extractor_url_2))
+# print(id(extractor_url))
+
+# 'is' deve ser usado sempre e somente quando queremos comparar a identidade de um objeto, isto é, seu endereço na memória.
+# O '==', por sua vez, deve ser usado quando quisermos comparar valores (ou, na verdade, qualquer outra coisa!) de objetos.
+
