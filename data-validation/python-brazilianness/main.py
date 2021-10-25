57faa9e386908185cec942ef6a2e2350defee4da
Igorswrk <igor.paixa@aluno.ufop.edu.br>
2021-10-25 20:53:28 -0300

    iniciando mod8-cap3 validacao de dados br

--- data-validation/python-brazilianness/main.py
+++ data-validation/python-brazilianness/main.py
@@ -1,4 +1,17 @@
-from cpf import Cpf
+from create_docs import Document
 
-cpf_user = Cpf("09962725690")
-print(cpf_user)
+from validate_docbr import CPF
+from validate_docbr import CNPJ
+
+cnpj = CNPJ()
+cpf = CPF()
+
+# cpf_user = Cpf("09962725690")
+# print(cpf_user)
+
+new_cpnj = cnpj.generate()
+new_cpf = cpf.generate()
+
+document = Document.create_new(new_cpnj)
+print(document)
+
