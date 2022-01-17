46d036b1f1ae107b9c140d688049a5e164edf958
Igorswrk <Igorsantiago.work@gmail.com>
2022-01-16 22:45:00 -0300

    finalizando mod8-cap5

--- tdd-python/src/auction/domain.py
+++ tdd-python/src/auction/domain.py
@@ -1,42 +1,51 @@
 import sys
 
-class User:
 
-    def __init__(self, name):
+class User:
+    def __init__(self, name, wallet):
         self.__name = name
+        self.__wallet = wallet
 
     @property
     def name(self):
         return self.__name
 
+    @property
+    def wallet(self):
+        return self.__wallet
+
+    def propose_bidding(self, auction, value):
+        if value > self.__wallet:
+            raise ValueError('Não pode propor um lance com valor maior que o valor da carteira')
+        bidding = Bidding(self, value)
+        auction.add_bidding(bidding)
+
+        self.__wallet -= value
 
 class Bidding:
-
     def __init__(self, user, value):
         self.user = user
         self.value = value
 
 
 class Auction:
-
     def __init__(self, description):
         self.description = description
         self.__biddings = []
-
-    @property
-    def biddings(self):
-        return self.__biddings
-
-
-class Evaluator:
-
-    def __init__(self):
         self.highest_bidding = sys.float_info.min
         self.lowest_bidding = sys.float_info.max
 
-    def evaluate(self, auction:Auction):
-        for i in auction.biddings:
-            if i.value > self.highest_bidding:
-                self.highest_bidding = i.value
-            if i.value < self.lowest_bidding:
-                self.lowest_bidding = i.value
+    def add_bidding(self, bidding: Bidding):
+        if not self.__biddings or self.__biddings[-1].user != bidding.user and bidding.value > self.__biddings[-1].value:
+            if bidding.value > self.highest_bidding:
+                self.highest_bidding = bidding.value
+            if bidding.value < self.lowest_bidding:
+                self.lowest_bidding = bidding.value
+
+            self.__biddings.append(bidding)
+        else:
+            raise ValueError('Erro ao propor lance!')
+
+    @property
+    def biddings(self):
+        return self.__biddings[:]
