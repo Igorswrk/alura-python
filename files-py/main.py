acbe3840a36c00c31cb81739eb614bfd2d78dba2
Igorswrk <Igorsantiago.work@gmail.com>
2022-01-24 22:47:42 -0300

    finalizando o mod10-cap4 curso i/o

--- files-py/main.py
+++ files-py/main.py
@@ -1,10 +1,14 @@
-contacts_file = open('font_files/contacts.csv', encoding='latin_1')
+try:
+   with open('font_files/contacts.csv', encoding='latin_1') as contacts_file:
+        for line in contacts_file: # Nao gasta muita memória pois não salva o arquivo todo
+            print(line, end='')
+except FileNotFoundError:
+    print("file not found")
+except PermissionError:
+    print("no write permission")
 
 """
 contacts_file.readlines() -> ler todas as linhas do arquivo
 contacts_file.readline() -> ler 1 linha por vez do arquivo
 
 """
-
-for line in contacts_file: # Nao gasta muita memória pois não salva o arquivo todo
-    print(line, end='')
