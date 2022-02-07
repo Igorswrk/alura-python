e1fec49a91eb7e0730e1622f2465d5bf1e4d28b0
Igorswrk <Igorsantiago.work@gmail.com>
2022-02-07 13:57:43 -0300

    finalizando mod pep8

--- pep8-typing-python/normal_queue.py
+++ pep8-typing-python/normal_queue.py
@@ -1,26 +1,14 @@
-class NormalQueue:
-    code: int = 0
-    queue = []
-    customers_served = []
-    current_password: str = ''
+from base_queue import BaseQueue
 
+from constants import NORMAL_CODE
+
+
+class NormalQueue(BaseQueue):
     def generate_current_password(self) -> None:
-        self.current_password = f'NM{self.code}'
-
-    def reset_queue(self) -> None:
-        if self.code >= 100:
-            self.code = 0
-        else:
-            self.code += 1
-
-    def update_queue(self) -> None:
-        self.reset_queue()
-        self.generate_current_password()
-        self.queue.append(self.current_password)
+        self.current_password = f'{NORMAL_CODE}{self.code}'
 
     def call_customer(self, cashier: int) -> str:
         current_customer = self.queue.pop(0)
         self.customers_served.append(current_customer)
 
         return f'Current customer: {current_customer}, go to cashier {cashier}'
-
--- pep8-typing-python/priority_queue.py
+++ pep8-typing-python/priority_queue.py
@@ -1,37 +1,20 @@
-class PriorityQueue:
-    code: int = 0
-    queue = []
-    customers_served = []
-    current_password: str = ''
+from base_queue import BaseQueue
+from constants import PRIORITY_CODE
+from typing import Union
 
+from summary_statistic import SummaryStatistic
+from detailed_statistic import DetailedStatistic
+
+Types_ = Union[SummaryStatistic, DetailedStatistic]
+
+class PriorityQueue(BaseQueue):
     def generate_current_password(self) -> None:
-        self.current_password = f'PR{self.code}'
-
-    def reset_queue(self) -> None:
-        if self.code >= 100:
-            self.code = 0
-        else:
-            self.code += 1
-
-    def update_queue(self) -> None:
-        self.reset_queue()
-        self.generate_current_password()
-        self.queue.append(self.current_password)
+        self.current_password = f'{PRIORITY_CODE}{self.code}'
 
     def call_customer(self, cashier: int) -> str:
         current_customer = self.queue.pop(0)
         self.customers_served.append(current_customer)
-
         return f'Current customer: {current_customer}, go to cashier {cashier}'
 
-    def statistic(self, day: str, bank_agency: int, flag: str) -> dict:
-         if flag != 'detail':
-             statistic = {f'{bank_agency} - {day} : {len(self.customers_served)}'}
-         else:
-             statistic = {}
-             statistic['day'] = day
-             statistic['bank agency'] = bank_agency
-             statistic['customers served'] = self.customers_served
-             statistic['quantity customers served'] = len(self.customers_served)
-
-         return statistic
+    def statistic(self, return_statistic: Types_) -> dict:
+        return return_statistic.wheel_statistic(self.customers_served)
